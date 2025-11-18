8f43db78f5ee860a8ae37a8e58b92d7fe9ede136
satriaxdev <satriadeveloperz@gmail.com>
2025-11-18 19:13:13 +0700

    Add database/config.js

---
+++ database/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8586933174:AAHzumcCbWajUhOQUc6VF__i1iiI862URfM",  // Masukin Bot token kamu
+  owners: "7961092652", // Masukin ID Telegram kamu
+  port: "3003", // Masukin Port panel kamu 
+  ipvps: "http://aii.sigma-private.com" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 1710-1711 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
